3e63ef55cb782a092b6fc75dbc4be0d3c0bca329
roncofaber <fabrice.roncoroni@gmail.com>
2026-06-18 10:18:07 -0700

    feat: add shared PubChem HTTP client

---
+++ internal/resolver/client.go
@@ -0,0 +1,145 @@
+package resolver
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"net/url"
+	"regexp"
+	"strings"
+	"time"
+)
+
+const pubchemBase = "https://pubchem.ncbi.nlm.nih.gov/rest/pug"
+const propertyFields = "IUPACName,MolecularFormula,MolecularWeight,InChIKey,CanonicalSMILES,IsomericSMILES"
+
+var errNotFound = errors.New("not found")
+var casRE = regexp.MustCompile(`^\d+-\d+-\d+$`)
+
+type pubchemClient struct {
+	baseURL string
+	http    *http.Client
+}
+
+func newPubchemClient() *pubchemClient {
+	return &pubchemClient{
+		baseURL: pubchemBase,
+		http:    &http.Client{Timeout: 15 * time.Second},
+	}
+}
+
+type propertyRow struct {
+	CID              int    `json:"CID"`
+	IUPACName        string `json:"IUPACName"`
+	MolecularFormula string `json:"MolecularFormula"`
+	MolecularWeight  string `json:"MolecularWeight"`
+	InChIKey         string `json:"InChIKey"`
+	CanonicalSMILES  string `json:"CanonicalSMILES"`
+	IsomericSMILES   string `json:"IsomericSMILES"`
+}
+
+type propertyTable struct {
+	Properties []propertyRow `json:"Properties"`
+}
+
+type propertyResponse struct {
+	PropertyTable propertyTable `json:"PropertyTable"`
+}
+
+type synonymEntry struct {
+	CID     int      `json:"CID"`
+	Synonym []string `json:"Synonym"`
+}
+
+type synonymInfo struct {
+	Information []synonymEntry `json:"Information"`
+}
+
+type synonymResponse struct {
+	InformationList synonymInfo `json:"InformationList"`
+}
+
+// fetchProperties calls PubChem for compound properties.
+// SMILES uses POST to handle special characters; name/CAS uses GET.
+func (c *pubchemClient) fetchProperties(namespace, identifier string, namespaceIsSmiles bool) (propertyResponse, error) {
+	path := fmt.Sprintf("%s/compound/%s/property/%s/JSON", c.baseURL, namespace, propertyFields)
+
+	var (
+		resp *http.Response
+		err  error
+	)
+	if namespaceIsSmiles {
+		body := strings.NewReader(url.Values{"smiles": {identifier}}.Encode())
+		resp, err = c.http.Post(path, "application/x-www-form-urlencoded", body)
+	} else {
+		getURL := fmt.Sprintf("%s/compound/%s/%s/property/%s/JSON",
+			c.baseURL, namespace, url.PathEscape(identifier), propertyFields)
+		resp, err = c.http.Get(getURL)
+	}
+	if err != nil {
+		return propertyResponse{}, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusNotFound {
+		return propertyResponse{}, errNotFound
+	}
+	if resp.StatusCode != http.StatusOK {
+		return propertyResponse{}, fmt.Errorf("pubchem returned %d", resp.StatusCode)
+	}
+	var result propertyResponse
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return propertyResponse{}, fmt.Errorf("decoding property response: %w", err)
+	}
+	return result, nil
+}
+
+// fetchSVG fetches the 2D structure SVG for a CID. Failure is non-fatal.
+func (c *pubchemClient) fetchSVG(cid int) (template.HTML, error) {
+	u := fmt.Sprintf("%s/compound/cid/%d/record/SVG?record_type=2d", c.baseURL, cid)
+	resp, err := c.http.Get(u)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("svg fetch returned %d", resp.StatusCode)
+	}
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return template.HTML(b), nil
+}
+
+// fetchCAS scans PubChem synonyms for the first CAS registry number.
+func (c *pubchemClient) fetchCAS(cid int) (string, error) {
+	u := fmt.Sprintf("%s/compound/cid/%d/synonyms/JSON", c.baseURL, cid)
+	resp, err := c.http.Get(u)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode == http.StatusNotFound {
+		return "", nil
+	}
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("synonyms fetch returned %d", resp.StatusCode)
+	}
+	var sr synonymResponse
+	if err := json.NewDecoder(resp.Body).Decode(&sr); err != nil {
+		return "", err
+	}
+	if len(sr.InformationList.Information) == 0 {
+		return "", nil
+	}
+	for _, syn := range sr.InformationList.Information[0].Synonym {
+		if casRE.MatchString(syn) {
+			return syn, nil
+		}
+	}
+	return "", nil
+}
---
+++ internal/resolver/client_test.go
@@ -0,0 +1,82 @@
+package resolver
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestFetchProperties_Success(t *testing.T) {
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		want := "/compound/smiles/property/IUPACName,MolecularFormula,MolecularWeight,InChIKey,CanonicalSMILES,IsomericSMILES/JSON"
+		if r.URL.Path != want {
+			http.Error(w, "unexpected path: "+r.URL.Path, 404)
+			return
+		}
+		json.NewEncoder(w).Encode(propertyResponse{
+			PropertyTable: propertyTable{
+				Properties: []propertyRow{{
+					CID: 180, IUPACName: "propan-2-one",
+					MolecularFormula: "C3H6O", MolecularWeight: "58.08",
+					InChIKey: "CSCPPACGZOOCGX-UHFFFAOYSA-N",
+					CanonicalSMILES: "CC(C)=O", IsomericSMILES: "CC(C)=O",
+				}},
+			},
+		})
+	}))
+	defer srv.Close()
+
+	c := &pubchemClient{baseURL: srv.URL, http: srv.Client()}
+	got, err := c.fetchProperties("smiles", "CC(C)=O", true)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(got.PropertyTable.Properties) == 0 {
+		t.Fatal("expected at least one property row")
+	}
+	if got.PropertyTable.Properties[0].IUPACName != "propan-2-one" {
+		t.Errorf("got %q, want %q", got.PropertyTable.Properties[0].IUPACName, "propan-2-one")
+	}
+}
+
+func TestFetchProperties_NotFound(t *testing.T) {
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "not found", 404)
+	}))
+	defer srv.Close()
+
+	c := &pubchemClient{baseURL: srv.URL, http: srv.Client()}
+	_, err := c.fetchProperties("smiles", "INVALID", true)
+	if err != errNotFound {
+		t.Errorf("got %v, want errNotFound", err)
+	}
+}
+
+func TestFetchCAS(t *testing.T) {
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != fmt.Sprintf("/compound/cid/%d/synonyms/JSON", 180) {
+			http.Error(w, "unexpected path", 404)
+			return
+		}
+		json.NewEncoder(w).Encode(synonymResponse{
+			InformationList: synonymInfo{
+				Information: []synonymEntry{{
+					CID:     180,
+					Synonym: []string{"acetone", "67-64-1", "2-propanone"},
+				}},
+			},
+		})
+	}))
+	defer srv.Close()
+
+	c := &pubchemClient{baseURL: srv.URL, http: srv.Client()}
+	cas, err := c.fetchCAS(180)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if cas != "67-64-1" {
+		t.Errorf("got %q, want %q", cas, "67-64-1")
+	}
+}
