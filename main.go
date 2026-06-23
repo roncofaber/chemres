1d8f29732ce87bf3e0f844e108665f494b5ef8f6
roncofaber <fabrice.roncoroni@gmail.com>
2026-06-23 12:03:39 -0700

    feat: wire /batch/start and /batch/stream routes

--- main.go
+++ main.go
@@ -6,19 +6,22 @@ import (
 	"os"
 
 	"github.com/roncofaber/chemres/internal/handlers"
+	"github.com/roncofaber/chemres/internal/jobs"
 	"github.com/roncofaber/chemres/internal/resolver"
 )
 
 func main() {
-	tmpl := handlers.MustLoadTemplates("templates")
-	r    := resolver.NewAutoResolver()
+	tmpl  := handlers.MustLoadTemplates("templates")
+	r     := resolver.NewAutoResolver()
+	store := jobs.NewStore()
 
 	mux := http.NewServeMux()
-	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	mux.Handle("/export",  handlers.NewExportHandler(tmpl))
-	mux.Handle("/resolve", handlers.NewResolveHandler(tmpl, r))
-	mux.Handle("/batch",   handlers.NewBatchHandler(tmpl, r))
-	mux.Handle("/suggest", handlers.NewSuggestHandler(tmpl, r))
+	mux.Handle("/static/",      http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	mux.Handle("/export",       handlers.NewExportHandler(tmpl))
+	mux.Handle("/resolve",      handlers.NewResolveHandler(tmpl, r))
+	mux.Handle("/batch/start",  handlers.NewBatchStartHandler(tmpl, r, store))
+	mux.Handle("/batch/stream", handlers.NewBatchStreamHandler(tmpl, r, store))
+	mux.Handle("/suggest",      handlers.NewSuggestHandler(tmpl, r))
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		if req.URL.Path != "/" {
